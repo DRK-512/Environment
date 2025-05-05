d89de1f7937163b5e3e175a999a05dad88ca99a9
drk <drk@pop-os.attlocal.net>
2025-05-04 22:44:17 -0400

    missed some apps

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
-sudo apt install git cmake build-essential code vim neovim gimp wireshark tcpdump net-tools gparted openssh-client openssh-server nmap -y
+sudo apt install git curl wget cmake build-essential code vim neovim gimp wireshark tcpdump net-tools gparted openssh-client openssh-server nmap chromium-browser tmux -y
+
 # Add docker config
 # append .bashrc with git branch
 # Terminal color preferences
 # neovim config
 # Window manager
+# Setup gnome default apps
